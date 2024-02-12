7d99f06c17d14ab72212e748dcd6f5b859ea139b
Austin Shinkle <austinshinkle@gmail.com>
2024-02-12 21:06:59 +0100

    basic integration of HX711 library and update to readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This repository will contain the code that will measure the level of beer remaining in the kegs
+
+# Notes
+I had to uninstall the GPIO package and install lgpio since GPIO is not natively supported for RPI5
+
+```pip3 uninstall rpi-lgpio
+sudo apt install python3-rpi.gpio
+```
--- src/KegLevel.py
+++ src/KegLevel.py
@@ -1,1 +1,28 @@
-# placeholder for the main python file
+from hx711 import HX711
+import RPi.GPIO as GPIO
+from time import sleep
+
+try:
+	
+	while True:
+
+		try:
+			hx711 = HX711(
+				dout_pin=5,
+				pd_sck_pin=6,
+				channel='A',
+				gain=128
+			)
+
+			hx711.reset()   # Before we start, reset the HX711 (not obligate)
+			measures = hx711.get_raw_data(times=5)
+		finally:
+			GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+
+		print(measures)
+    
+		sleep(1)
+	
+	
+except KeyboardInterrupt:
+	print('Script cancelled by user!')
