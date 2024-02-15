b28ea4d94961e8ab632708dfda2df49a97e072dd
Austin Shinkle <austinshinkle@gmail.com>
2024-02-15 21:08:15 +0100

    added offset

--- src/KegLevel.py
+++ src/KegLevel.py
@@ -1,9 +1,16 @@
 from hx711 import HX711
 import RPi.GPIO as GPIO
 from time import sleep
+from gpiozero import LED
 
 # samples that should be taken for each sensor
 SAMPLES = 5
+
+OFFSET_SCALE_1 = -519856.2
+OFFSET_SCALE_2 = -640729.2
+
+led_green = LED(12)
+led_red = LED(25)
 
 try:
 	
@@ -43,9 +50,11 @@ try:
 				num += 1
 			sensor_1_avg /= SAMPLES
 			
-			#print(measures)
-			#print(f"The average of sensor 1 = {sensor_1_avg}\n")
+			# apply offset
+			sensor_1_avg = int(sensor_1_avg - OFFSET_SCALE_1)
 			
+			
+						
 			measures_2 = hx711_2.get_raw_data(times=SAMPLES)
 			
 			num = 0
@@ -54,12 +63,20 @@ try:
 				num += 1
 			sensor_2_avg /= SAMPLES
 			
-			#print(measures_2)
-			#print(f"The average of sensor 2 = {sensor_2_avg}\n")
+			# apply offset
+			sensor_2_avg = int(sensor_2_avg - OFFSET_SCALE_2)
+			
+			if sensor_1_avg < 0:
+				led_red.on();
+				led_green.off()
+			else:
+				led_red.off();
+				led_green.on()	
+			
 
 			print(f"{sensor_1_avg},{sensor_2_avg}")
     
-			sleep(1)
+			sleep(.5)
 		
 		# add a GPIO exception here
 		except KeyboardInterrupt:
