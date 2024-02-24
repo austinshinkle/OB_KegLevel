6ca93349198a7509b45b27183db84ccfa88e8fd0
Austin Shinkle <austinshinkle@gmail.com>
2024-02-24 17:36:17 +0100

    added dynamic pulling of the beer info

--- src/KegLevel.py
+++ src/KegLevel.py
@@ -9,6 +9,8 @@ from gpiozero import LED
 
 # display imports
 from nicegui import run, ui, app
+import requests
+import json
 
 # samples that should be taken for each sensor
 SAMPLES = 5
@@ -25,6 +27,7 @@ SENSOR_2_SCALE = 1/600 #temp value
 CSS_HEADING_H1 = 'color: #111111; font-variant: small-caps; font-size: xxx-large; font-weight: 500; font-family: Andale Mono, monospace'
 CSS_HEADING_H2 = 'color: #222222; font-variant: small-caps; font-size: xx-large; font-weight: 500; font-family: Andale Mono, monospace'
 CSS_LABEL = 'color: #333333; font-variant: small-caps; font-size: x-large; font-family: Andale Mono, monospace'
+CSS_LABEL_SMALL = 'color: #333333; font-variant: small-caps; font-size: medium; font-family: Andale Mono, monospace'
 
 # on tap images
 ON_TAP_KEG_1_IMAGE = 'https://static.wixstatic.com/media/22ae94_0e87a582058c47d1ba8227377bd4b334~mv2.png'
@@ -34,6 +37,16 @@ ON_TAP_KEG_2_IMAGE = 'https://static.wixstatic.com/media/d3a926_41fdde2b7fce45c8
 terminate_thread = False
 sensor_1_pct = 0
 sensor_2_pct = 0
+
+# global variables for web site api calls
+tap1_beer_name = ''
+tap1_abv = ''
+tap1_ibu = ''
+tap1_image_url = ''
+tap2_beer_name = ''
+tap2_abv = ''
+tap2_ibu = ''
+tap2_image_url = ''
 
 
 led_green = LED(12)
@@ -97,7 +110,95 @@ def measure_kegs():
 		#print(f"{sensor_1_pct}%,{sensor_2_pct}%")
 
 		sleep(.5)
+		
+def get_on_tap_info():
+	
+	global tap1_beer_name
+	global tap1_abv
+	global tap1_ibu
+	global tap1_image_url
+	global tap2_beer_name
+	global tap2_abv
+	global tap2_ibu
+	global tap2_image_url
+	
+	global terminate_thread
+	
+	api_key = "IST.eyJraWQiOiJQb3pIX2FDMiIsImFsZyI6IlJTMjU2In0.eyJkYXRhIjoie1wiaWRcIjpcIjI4ODk4OTNjLWEwNTgtNGYyZS1hZTljLWZkOTc4NTAyZTY1YVwiLFwiaWRlbnRpdHlcIjp7XCJ0eXBlXCI6XCJhcHBsaWNhdGlvblwiLFwiaWRcIjpcImUyZWRlNzQ2LTk3NGItNGU5MC05YjViLThlMWFhMTgxOTcxM1wifSxcInRlbmFudFwiOntcInR5cGVcIjpcImFjY291bnRcIixcImlkXCI6XCJkM2E5MjY4YS1hYmNhLTRkMDEtOTg5MC05MmJjM2E5YThmZGZcIn19IiwiaWF0IjoxNzA4NzU2Mjc0fQ.RG1RtK-aujdflcHiLK7NZ8BjkjLrC8nuoDPayCkyZ0QE1ZPXCX3BsngKVEmGh6KOZ_Gb58bQJ-d-PUa9WtAwRf3oi756qWpwg8VhWurfJag795rj7qMdV39mbzEU3jIN_HvxZn-T6Lr0dLYQw1Wc1cUgrTDUovJ3EYkug0P8T7M5KMToRovz-fxRaqphGUY1WkuHWEpVAYu5xyVeKAmMkqdy5F3xoR-8nhSjWswtJvYYO5qKPvPfEW5G1HZCsZEWwnk-tlkU0EbC2v68hzZlJmJ3ChV2h4PfR00Y4ks12WuUinEaOoIxFVi2jM32i-7WDtvsB_TlVHKOhKKrsf7eMQ"
 
+	account_id = "d3a9268a-abca-4d01-9890-92bc3a9a8fdf"
+	site_id = "a8995e51-a20f-4a5b-88ca-720ed93eb1a7"
+
+	query_endpoint = 'https://www.wixapis.com/wix-data/v2/items/query'
+	image_base_url = 'https://static.wixstatic.com/media/'
+
+	query_data = {
+	  "dataCollectionId": "BeerRecipes"
+	}
+
+	# define the header
+	headers = {
+		#'Content-Type': 'application/json',
+		'Authorization': f'{api_key}',
+		#'wix-account-id': f'{account_id}',
+		'wix-site-id': f'{site_id}'  # Example content type
+	}
+
+	while not terminate_thread:
+	
+		response = requests.post(query_endpoint, headers=headers, params=query_data)
+
+		# Check if the request was successful (status code 200)
+		if response.status_code == 200:
+			# Parse the JSON response
+			data = response.json()
+			
+			element_count = data['pagingMetadata']['count']
+
+			print(f"I found {element_count} elements")
+			
+			# Access the parsed data (as a Python dictionary)
+			num = 0
+			while num < element_count:
+				
+				#find information for Tap 1
+				if data['dataItems'][num]['data']['onTap'] == 'Tap1':
+
+					tap1_beer_name = data['dataItems'][num]['data']['title']            
+					tap1_abv = data['dataItems'][num]['data']['actualAbv']
+					tap1_ibu = data['dataItems'][num]['data']['calculatedIbu']
+					
+					# get the image string and split the string by "/" and then put the full url together
+					parts = data['dataItems'][num]['data']['image'].split("/")
+					tap1_image_url = image_base_url + parts[3]
+					
+					print(tap1_beer_name)
+					print(tap1_abv)
+					print(tap1_ibu)
+					print(tap1_image_url)
+					
+				if data['dataItems'][num]['data']['onTap'] == 'Tap2':
+
+					#find information for Tap 2
+					tap2_beer_name = data['dataItems'][num]['data']['title']            
+					tap2_abv = data['dataItems'][num]['data']['actualAbv']
+					tap2_ibu = data['dataItems'][num]['data']['calculatedIbu']
+					
+					# get the image string and split the string by "/" and then put the full url together
+					parts = data['dataItems'][num]['data']['image'].split("/")
+					tap2_image_url = image_base_url + parts[3]
+					
+					print(tap2_beer_name)
+					print(tap2_abv)
+					print(tap2_ibu)
+					print(tap2_image_url)
+					
+				num += 1
+				
+		else:
+			print(f'Error: {response.status_code}')
+			
+		sleep(15);
 
 try:
 	
@@ -125,31 +226,46 @@ try:
 	thread = threading.Thread(target=measure_kegs)
 	thread.start()
 	
+	thread2 = threading.Thread(target=get_on_tap_info)
+	thread2.start()
+	
 	### FIX THIS LATER!!!! (not a good implementation)
 	sleep(5)
 	
 	# setup up the UI for the web page
 	with ui.row():
 		ui.image('../media/Ostentatious Brewing - Robot 2.jpeg').style("width: 100px")
-		ui.label('CSS').style(CSS_HEADING_H1).set_text("Ostententatious Brewing!")
+		ui.link('Ostentatious Brewing', 'https://ostentatiousbrewing.wixsite.com/ostentatiousbrewing', new_tab=True).style(CSS_HEADING_H1)
 	ui.label('CSS').style(CSS_HEADING_H2).set_text("This is what is on tap!")
 	with ui.row():
 		with ui.card():
 			ui.label('CSS').style(CSS_HEADING_H2).set_text("Tap 1")
-			ui.image(ON_TAP_KEG_1_IMAGE).style("width: 250px")
-			keg_1 = ui.label('CSS').style(CSS_LABEL)
+			ui_tap1_image = ui.image(tap1_image_url).style("width: 250px")
+			ui_tap1_abv = ui.label('CSS').style(CSS_LABEL_SMALL).set_text(f"{tap1_abv} ABV")
+			ui_tap1_ibu = ui.label('CSS').style(CSS_LABEL_SMALL).set_text(f"{tap1_ibu} IBU")
+			ui_tap1_pct_beer = ui.label('CSS').style(CSS_LABEL)
 		with ui.card():
 			ui.label('CSS').style(CSS_HEADING_H2).set_text("Tap 2")		
-			ui.image(ON_TAP_KEG_2_IMAGE).style("width: 250px")
-			keg_2 = ui.label('CSS').style(CSS_LABEL)
-	ui.link('Ostentatious Brewing Website', 'https://ostentatiousbrewing.wixsite.com/ostentatiousbrewing', new_tab=True).style(CSS_LABEL)
+			ui_tap2_image = ui.image(tap2_image_url).style("width: 250px")
+			ui_tap2_abv = ui.label('CSS').style(CSS_LABEL_SMALL).set_text(f"{tap2_abv} ABV")
+			ui_tap2_ibu = ui.label('CSS').style(CSS_LABEL_SMALL).set_text(f"{tap2_ibu} IBU")
+			ui_tap2_pct_beer = ui.label('CSS').style(CSS_LABEL)
 	
 	# update UI elements on a timer
-	ui.timer(1.0, lambda: keg_1.set_text(f'{sensor_1_pct}% Beer Remaining'))
-	ui.timer(1.0, lambda: keg_2.set_text(f'{sensor_2_pct}% Beer Remaining'))
+	ui.timer(1.0, lambda: ui_tap1_pct_beer.set_text(f'{sensor_1_pct}% Beer Remaining'))
+	ui.timer(1.0, lambda: ui_tap2_pct_beer.set_text(f'{sensor_2_pct}% Beer Remaining'))
+	
+	### TODO: TURN THIS GARBAGE INTO A PROPER IMPLEMENTATION :) ###
+#	ui.timer(15.0, lambda: ui_tap1_image.set_source(tap1_image_url)) 
+#	ui.timer(15.0, lambda: ui_tap1_abv.set_text(f"{tap1_abv} ABV")) 
+#	ui.timer(15.0, lambda: ui_tap1_ibu.set_text(f"{tap1_ibu} IBU")) 
+#	ui.timer(15.0, lambda: ui_tap2_image.set_source(tap2_image_url)) 
+#	ui.timer(15.0, lambda: ui_tap2_abv.set_text(f"{tap2_abv} ABV")) 
+#	ui.timer(15.0, lambda: ui_tap2_ibu.set_text(f"{tap2_ibu} IBU")) 
+	
 	
 	# run the UI
-	ui.run(reload=False, title="Ostentatious Brewing")
+	ui.run(reload = False, title="Ostentatious Brewing")
 	
 	# Wait for keyboard input to terminate the thread
 	input("Press Enter to stop the program..\n")
